1c173e06018df8a5dec457199364117eacd73344
MarcinKowalski89 <marcinkowalski189@gmail.com>
2016-12-16 10:27:49 +0100

    Setting singleton

--- classes/Search.php
+++ classes/Search.php
@@ -2,13 +2,18 @@
 
 class Search {
 
-    public static function getInstance() {
-        // should return Search singleton
-    }
+   protected static $instance;
+
+   public static function getInstance() {
+       if(self::$instance === null) {
+           self::$instance = new Search();
+       }
+       return self::$instance;
+   }
     
     public function run(IFilter $filter, Employe $employee) {
         // should return array of employees matching the filter condition
-        // starting from given employee in organizational hierarchy
+        // starting from given employee in organizational hierarchy       
     }
 
 }
