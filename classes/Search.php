e0f5b9075619356304f021df1a9c98d035b2c50f
MarcinKowalski89 <marcinkowalski189@gmail.com>
2017-01-18 08:30:36 +0100

    Attempt to end Search

--- classes/Search.php
+++ classes/Search.php
@@ -11,9 +11,21 @@ class Search {
        return self::$instance;
    }
     
-    public function run(IFilter $filter, Employe $employee) {
-        // should return array of employees matching the filter condition
-        // starting from given employee in organizational hierarchy    
+    /**
+     * Run a search on hierarchy of employees using given filter as criterion
+     *
+     * @param IFilter $filter to use as search criterion
+     * @param Employee $employee being tip of a hierarchy to start searching from
+     * @return Employee[]
+     */
+    public function run(IFilter $filter, Employee $employee) {
+        static $found = [];
+        if ($filter->match($employee)) {
+            $found[] = $employee;
+        }
+        foreach ($employee->getSubordinates() as $subordinate) {
+            $found += Search::run($filter, $subordinate);
+        }
+        return $found;
     }
-
-}
+} 
